7c12e995acb0b109f2b203cd5af854eb35aee74c
Nwankwo Chidera <nwankwochidera.david@gmail.com>
2024-08-30 23:30:27 +0100

    fixed some issues

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
 const gameLinks = [];
+
+function delay(time) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, time);
+  });
+}
 
 async function extractMatchInfo(page) {
   // root link
@@ -216,110 +223,141 @@ async function statistics(page) {
         }
       }
     }
+    return;
   } catch (error) {
     console.log("an error occurred: ", error);
+    return;
   }
 }
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
 
-  // Get today's day
-  const today = new Date().getDay();
+    // Get today's day
+    const today = new Date().getDay();
 
-  // Define time based on today's day
-  let time;
-  switch (today) {
-    case 0:
-      time = 0;
-      break; // Sunday
-    case 1:
-      time = 1;
-      break; // Monday
-    case 2:
-      time = 2;
-      break; // Tuesday
-    case 3:
-      time = 3;
-      break; // Wednesday
-    case 4:
-      time = 4;
-      break; // Thursday
-    case 5:
-      time = 5;
-      break; // Friday
-    case 6:
-      time = 6;
-      break; // Saturday
-    default:
-      time = 0;
-      break; // Default to Sunday
-  }
-
-  // Construct URL
-  const tomorrowUrl = `https://www.sportybet.com/ng/m/sport/football?time=${time}&sort=0`;
-  //   const tomorrowUrl = `https://www.sportybet.com/ng/m/sport/football?time=${time +
-  //     1}&sort=0`;
-
-  // Open tomorrow's URL
-  await page.goto(tomorrowUrl);
-
-  // Define the scroll function
-  const scrollUntilElementVisible = async () => {
-    await page.evaluate(() => {
-      window.scrollBy(0, 100); // Scroll down by 100 pixels
-    });
-    await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second for the page to settle
-  };
-
-  let count = 0;
-  let links = [];
-  // Keep scrolling until the element is visible
-  while (count < 20) {
-    // Extract the links
-    await scrollUntilElementVisible();
-    let link = await extractMatchInfo(page);
-    for (let i = 0; i < link.length; i++) {
-      // Extract data key attribute
-      const dataKey = link[i].dataKey;
-
-      // Check if a match with the same data key already exists
-      const existingMatch = links.find(match => match.dataKey === dataKey);
-      if (!existingMatch) {
-        links.push(link[i]);
-      }
+    // Define time based on today's day
+    let time;
+    switch (today) {
+      case 0:
+        time = 0;
+        break; // Sunday
+      case 1:
+        time = 1;
+        break; // Monday
+      case 2:
+        time = 2;
+        break; // Tuesday
+      case 3:
+        time = 3;
+        break; // Wednesday
+      case 4:
+        time = 4;
+        break; // Thursday
+      case 5:
+        time = 5;
+        break; // Friday
+      case 6:
+        time = 6;
+        break; // Saturday
+      default:
+        time = 0;
+        break; // Default to Sunday
     }
-    links.push(...link);
-    await scrollUntilElementVisible();
-    count++;
-    // const stopLoadMore = await page.$(".bet-load-more-none");
-    // if (stopLoadMore) {
-    //   // The element is visible, stop scrolling
-    //   break;
-    // }
-  }
 
-  // Analysis is done here
-  for (let i = 0; i < links.length; i++) {
-    const newPage = await browser.newPage();
-    newPage.goto(links[i].link);
+    // Construct URL
+    // const tomorrowUrl = `https://www.sportybet.com/ng/m/sport/football?time=${time}&sort=0`;
+    // const tomorrowUrl = `https://www.sportybet.com/ng/m/sport/football/`;
+    // const tomorrowUrl = `https://www.sportybet.com/ng/m/sport/football/today?sort=0`;
+    // const tomorrowUrl = `https://www.sportybet.com/ng/m/sport/football?time=${time +
+    //   1}&sort=0`;
+    const tomorrowUrl = `https://www.sportybet.com/ng/m/sport/football?sort=1&time=${time + 1}`;
 
-    // Click on stat
-    try {
-      await newPage.waitForSelector(".m-icon.m-icon-stat", { timeout: 10000 });
-      await newPage.click(".m-icon.m-icon-stat");
-      try {
-        console.log("\n" + links[i].teams);
-        await statistics(newPage);
-      } catch (error) {
-        console.log(error);
+    // Open tomorrow's URL
+    await page.goto(tomorrowUrl);
+
+    // Define the scroll function
+    const scrollUntilElementVisible = async () => {
+      await page.evaluate(() => {
+        window.scrollBy(0, 100); // Scroll down by 100 pixels
+      });
+      await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second for the page to settle
+    };
+
+    //Links to open new page
+    let links = [];
+    //Link to save page that obeys the rule
+    let obeyLink = [];
+    // Keep scrolling until the element is visible
+    //Count
+    let count = 0;
+    while (true) {
+      count++;
+
+      // if (count % 4 === 0) {
+      //   console.log("Cool down starting");
+      //   await delay(500);
+      //   console.log("Cool down ended");
+      // }
+      // Extract the links
+      await scrollUntilElementVisible();
+      links = await extractMatchInfo(page);
+      if (links != undefined) {
+        for (let i = 0; i < links.length; i++) {
+          if (!obeyLink.includes(links[i].link)) {
+            // Analysis is done here
+            const newPage = await browser.newPage();
+            newPage.goto(links[i].link);
+            await newPage.waitForNavigation();
+            // Click on stat
+            try {
+              await newPage.waitForSelector(".m-icon.m-icon-stat", {
+                timeout: 10000
+              });
+              await newPage.click(".m-icon.m-icon-stat");
+              try {
+                console.log("\n" + links[i].teams);
+                await statistics(newPage);
+              } catch (error) {
+                console.log(error);
+              }
+            } catch (error) {
+              console.log(error);
+            }
+            if (newPage != undefined) {
+              await newPage.close();
+            }
+            obeyLink.push(links[i].link);
+          }
+        }
+        console.log("Obey Link: ", obeyLink);
+
+        console.log("Game Link Url: ", gameLinks);
       }
-    } catch (error) {
-      console.log(error);
+
+      await scrollUntilElementVisible();
+      const stopLoadMore = await page.$(".bet-load-more-none");
+      if (stopLoadMore) {
+        // The element is visible, stop scrolling
+        break;
+      }
+      fs.writeFile("gameLinks.txt", gameLinks.join("\n"), err => {
+        if (err) {
+          console.error("Error writing to file:", err);
+        } else {
+          console.log("Game links saved to gameLinks.txt");
+        }
+      });
     }
-    await newPage.close();
+    console.log("Obey Link: ", obeyLink);
+
+    console.log("Game Link Url: ", gameLinks);
+    // Write the gameLinks to a text file
+
+    await browser.close();
+  } catch (error) {
+    console.log("An error occurred:", error);
   }
-  console.log(gameLinks);
-  await browser.close();
 })();
